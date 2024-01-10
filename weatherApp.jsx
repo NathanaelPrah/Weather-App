ec8fe91a45d4628b5cbc2fc5d59f26484071106a
PrahNathanael <144918426+NathanaelPrah@users.noreply.github.com>
2024-01-10 13:09:45 +0000

    Update weatherApp.jsx

--- weatherApp.jsx
+++ weatherApp.jsx
@@ -2,15 +2,6 @@
 import React, { useState } from 'react'
 import "../Weather-App/weatherApp.css"
 
-// import search_icon from '../Assets/search.png';
-// import clear_icon from '../Assets/clear.png';
-// import cloud_icon from '../Assets/cloud.png';
-// import drizzle_icon from '../Assets/drizzle.png';
-// import rain_icon from '../Assets/rain.png';
-// import wind_icon from '../Assets/wind.png';
-// import humidity_icon from '../Assets/humidity.png';
-// import snow_icon from '../Assets/snow.png';
-
 const api = {
     key: "248c637765425af9c6018dc00892ea62",
     base: "https://api.openweathermap.org/data/2.5/",
@@ -67,39 +58,8 @@ const WeatherApp = () => {
             </header>
         </div>
     );
-    //     <div className='container'>
-    //         <div className='top-bar'>
-    //             <input type='text' className="cityInput" placeholder='Enter City /Town' />
-    //             <button className="search-icon" onClick={search} >
-    //                 <img src={search_icon} alt="" />
-    //                 <p>test</p>
-    //             </button>
-    //         </div>
-    //         <div className="weather-image">
-    //             <img src={wicon} alt="" />
-    //         </div>
-    //         <div className="weather-temp">24°c</div>
-    //         <div className="weather-location">Adenta</div>
-    //         <div className="date-container">
-    //             <div className="element">
-    //                 <img src={humidity_icon} alt='' className="icon" />
-    //                 <div className="data">
-    //                     <div className="humidity-percent">64%</div>
-    //                     <div className="text">Humidity</div>
-    //                 </div>
-    //             </div>
-    //             <div className="element">
-    //                 <img src={wind_icon} alt='' className="icon" />
-    //                 <div className="data">
-    //                     <div className="humidity-percent">18km/h</div>
-    //                     <div className="text">Wind Speed</div>
-    //                 </div>
-    //             </div>
-
-
-    //         </div>
-    //     </div>
-    // );
+    ssName="weather-image">
+   
 }
 
 export default WeatherApp;
